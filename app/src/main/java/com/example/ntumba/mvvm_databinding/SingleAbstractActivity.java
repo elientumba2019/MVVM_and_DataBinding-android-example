f22430f47f89d524d792bf284d49b7edaf467370
Elie nsampi ntumba <elientumba@live.fr>
2017-10-15 17:34:57 +0800

    Making the SingeAbstract activity abstract so that classes that extends it , uses its layout, this favorizes the use of fragments , furthermore it makes the app easy to maintain

--- app/src/main/java/com/example/ntumba/mvvm_databinding/SingleAbstractActivity.java
+++ app/src/main/java/com/example/ntumba/mvvm_databinding/SingleAbstractActivity.java
@@ -1,9 +1,20 @@
 package com.example.ntumba.mvvm_databinding;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SingleAbstractActivity extends AppCompatActivity {
+public abstract class SingleAbstractActivity extends AppCompatActivity {
+
+
+    /**
+     * returns some fragment from any activity
+     * that extends this class
+     * @return
+     */
+    public abstract Fragment createLayout();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
