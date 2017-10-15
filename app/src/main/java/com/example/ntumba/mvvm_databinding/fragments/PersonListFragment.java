7e10c1f4ae2925949d09df3f53d1dade1404db1f
Elie nsampi ntumba <elientumba@live.fr>
2017-10-15 18:47:34 +0800

    Implementing the ViewHolder class

--- app/src/main/java/com/example/ntumba/mvvm_databinding/fragments/PersonListFragment.java
+++ app/src/main/java/com/example/ntumba/mvvm_databinding/fragments/PersonListFragment.java
@@ -4,6 +4,8 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,7 +28,13 @@ public class PersonListFragment extends Fragment {
 
 
 
-
+    /**
+     * oncreate view method
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
@@ -38,8 +46,13 @@ public class PersonListFragment extends Fragment {
         );
 
 
+        //setting the layout manager
+        fragmentBinding.fragmentRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
         return fragmentBinding.getRoot();
     }
+
 
 
 
---
+++ app/src/main/java/com/example/ntumba/mvvm_databinding/list_utils/ListViewHolder.java
@@ -0,0 +1,20 @@
+package com.example.ntumba.mvvm_databinding.list_utils;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by ntumba on 17-10-15.
+ */
+
+public class ListViewHolder extends RecyclerView.ViewHolder {
+
+
+    /**
+     * constructor
+     * @param itemView
+     */
+    public ListViewHolder(View itemView) {
+        super(itemView);
+    }
+}
