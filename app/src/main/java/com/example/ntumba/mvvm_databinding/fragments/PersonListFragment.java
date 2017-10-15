b750e934b6a59832f2a8adc9b22cb7cfdf0f028c
Elie nsampi ntumba <elientumba@live.fr>
2017-10-15 18:16:37 +0800

    Adding the PersonListfragment corresponding to the FragmentListActivity

---
+++ app/src/main/java/com/example/ntumba/mvvm_databinding/fragments/PersonListFragment.java
@@ -0,0 +1,14 @@
+package com.example.ntumba.mvvm_databinding.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ntumba on 17-10-15.
+ */
+
+/**
+ * fragment corresponding to the
+ * FragmentListActivity.java
+ */
+public class PersonListFragment extends Fragment {
+}
