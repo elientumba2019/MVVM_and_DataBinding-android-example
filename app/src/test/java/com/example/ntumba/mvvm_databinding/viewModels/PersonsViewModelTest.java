bf89fdbfc6be87cdb902ba6149bc209145626e00
Elie nsampi <elientumba@live.fr>
2017-10-27 13:10:38 +0800

    Adding a test class for the viewholder class

---
+++ app/src/test/java/com/example/ntumba/mvvm_databinding/viewModels/PersonsViewModelTest.java
@@ -0,0 +1,15 @@
+package com.example.ntumba.mvvm_databinding.viewModels;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ntumba on 17-10-27.
+ */
+public class PersonsViewModelTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+}
