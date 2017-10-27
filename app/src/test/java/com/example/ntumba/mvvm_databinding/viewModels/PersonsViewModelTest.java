b44316ba03c06756a552ae8fa0480ef660949acb
Elie nsampi <elientumba@live.fr>
2017-10-27 13:11:27 +0800

    Adding a test class for the viewmodel class

--- app/src/test/java/com/example/ntumba/mvvm_databinding/viewModels/PersonsViewModelTest.java
+++ app/src/test/java/com/example/ntumba/mvvm_databinding/viewModels/PersonsViewModelTest.java
@@ -8,6 +8,15 @@ import static org.junit.Assert.*;
  * Created by ntumba on 17-10-27.
  */
 public class PersonsViewModelTest {
+
+
+    private So
+
+
+    /**
+     * setup for the sound view model
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception {
     }
